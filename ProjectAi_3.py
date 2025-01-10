5d060021ecf0780e2e2866cfe0f86a7f0fceaf18
nafise2082 <nafiseshamshiri82@gmail.com>
2025-01-10 12:49:01 +0330

    Update ProjectAi_3.py

--- ProjectAi_3.py
+++ ProjectAi_3.py
@@ -22,3 +22,53 @@ class ScheduleCSP:
                 return False
 
         return True
+ def select_unassigned_variable(self):
+        return min(
+            (var for var in self.variables if var not in self.assignment),
+            key=lambda var: len(self.domains[var])
+        )
+
+    def order_domain_values(self, var):
+        return sorted(self.domains[var], key=lambda value: self.conflicts(var, value))
+
+    def conflicts(self, course, value):
+        room, time, instructor = value
+        conflicts = 0
+        for assigned_course, assigned_value in self.assignment.items():
+            assigned_room, assigned_time, assigned_instructor = assigned_value
+            if time == assigned_time and room == assigned_room:
+                conflicts += 1
+            if time == assigned_time and instructor == assigned_instructor:
+                conflicts += 1
+        return conflicts
+
+    def forward_checking(self, course, value):
+        room, time, instructor = value
+        temp_domains = {var: list(domain) for var, domain in self.domains.items()}
+        for var in self.variables:
+            if var != course and var not in self.assignment:
+                temp_domains[var] = [val for val in self.domains[var] if self.is_consistent(var, val)]
+        return temp_domains
+
+    def backtrack(self):
+        if len(self.assignment) == len(self.variables):
+            return self.assignment
+
+        var = self.select_unassigned_variable()
+        for value in self.order_domain_values(var):
+            if self.is_consistent(var, value):
+                self.assignment[var] = value
+                original_domains = self.domains
+                self.domains = self.forward_checking(var, value)
+
+                result = self.backtrack()
+                if result:
+                    return result
+
+                self.assignment.pop(var)
+                self.domains = original_domains
+
+        return None
+
+    def solve(self):
+        return self.backtrack()
